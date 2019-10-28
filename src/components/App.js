254e00b6d1ed1deb248324d9f606ce4058ca286f
Vasileios Psychas <vaspsych@gmail.com>
2019-10-28 14:10:07 +0100

    Add some ids to our data

--- src/components/App.js
+++ src/components/App.js
@@ -9,14 +9,14 @@ class App extends React.Component {
     super(props);
     this.state = {
       items: [
-        { text: 'Wash my face!', done: false },
-        { text: 'Walk the dog', done: false },
-        { text: 'Pay the rent', done: false },
-        { text: 'Make so moneys', done: false },
-        { text: 'Make a website', done: true },
-        { text: 'Call my mom', done: true },
-        { text: 'Finish reading my book', done: true },
-        { text: 'Make more moneys', done: true }
+        { id: 0, text: 'Wash my face!', done: false },
+        { id: 1, text: 'Walk the dog', done: false },
+        { id: 2, text: 'Pay the rent', done: false },
+        { id: 3, text: 'Make so moneys', done: false },
+        { id: 4, text: 'Make a website', done: true },
+        { id: 5, text: 'Call my mom', done: true },
+        { id: 6, text: 'Finish reading my book', done: true },
+        { id: 7, text: 'Make more moneys', done: true }
       ]
     };
   }
