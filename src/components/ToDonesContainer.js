7edb57aae0f18b03829ea3de8fab4b977356590b
Vasileios Psychas <vaspsych@gmail.com>
2019-10-28 12:00:42 +0100

    Make America Functional Again

--- src/components/ToDonesContainer.js
+++ src/components/ToDonesContainer.js
@@ -1,32 +1,30 @@
 import React from 'react';
 
-class ToDonesContainer extends React.Component {
-  render() {
-    const todones = [
-      { text: 'Make a website', done: true },
-      { text: 'Call my mom', done: true },
-      { text: 'Finish reading my book', done: true },
-      { text: 'Make more moneys', done: true }
-    ];
+const ToDonesContainer = () => {
+  const todones = [
+    { text: 'Make a website', done: true },
+    { text: 'Call my mom', done: true },
+    { text: 'Finish reading my book', done: true },
+    { text: 'Make more moneys', done: true }
+  ];
 
-    const toDoNesItems = todones.map(el => {
-      return (
-        <div className="todones-item" key={el.text}>
-          <p>{el.text}</p>
-          <div className="actions">
-            <button className="btn">&#8635;</button>
-          </div>
-        </div>
-      );
-    });
-
+  const toDoNesItems = todones.map(el => {
     return (
-      <div className="todones-container">
-        <h3>BACKLOG</h3>
-        {todones.length > 0 && toDoNesItems}
+      <div className="todones-item" key={el.text}>
+        <p>{el.text}</p>
+        <div className="actions">
+          <button className="btn">&#8635;</button>
+        </div>
       </div>
     );
-  }
-}
+  });
+
+  return (
+    <div className="todones-container">
+      <h3>BACKLOG</h3>
+      {todones.length > 0 && toDoNesItems}
+    </div>
+  );
+};
 
 export default ToDonesContainer;
